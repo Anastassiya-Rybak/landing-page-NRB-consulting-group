c8f2db4062f869a745aeab8d44f028361b811482
Anastassiya Rybak <vyacheslavovna.an@mail.ru>
2022-10-18 13:14:44 +0600

    First commit

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+let consultation = document.getElementById('consultation-modal-window')
+
+function consult() {
+    consultation.style.display = 'flex'
+}
+function closeModal() {
+    consultation.style.display = 'none';
+}
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplayspeed: 3000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true
+  });
+  
