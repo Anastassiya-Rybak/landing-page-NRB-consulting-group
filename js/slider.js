a79171dc7201bc52faa4f113217f62d51623632d
Obliviate <vyacheslavovna.an@mail.ru>
2023-12-07 08:46:28 +0600

    fix: adaptive all

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,26 @@ $('.slider').slick({
     autoplay: true,
     autoplayspeed: 3000,
     pauseOnHover: true,
-    pauseOnDotsHover: true
+    pauseOnDotsHover: true,
+    responsive: [
+      {
+        breakpoint: 1045, // размер экрана 1024
+        settings: {
+          slidesToShow: 3, // тут меняем slidesToShow
+        }
+      },
+      {
+        breakpoint: 800, // размер экрана 600
+        settings: {
+          slidesToShow: 2, // тут меняем slidesToShow
+        }
+      },
+      {
+        breakpoint: 480, // размер экрана 480
+        settings: {
+          slidesToShow: 1, // тут меняем slidesToShow
+        }
+      }
+    ]
   });
   
